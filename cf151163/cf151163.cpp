f01c263887c03bb4885b8429c1ad32eb016ac891
PHATTHARANIT TESJAROEN <63010727@KMITL.AC.TH>
2020-11-15 15:50:00 +0700

    Add project files.

---
+++ cf151163/cf151163.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int x, y;
+int* p1 = &x;
+int* p2 = &y;
+void num() {
+	printf("input num1 num2 : ");
+	scanf("%d %d", &x, &y);
+	printf("&num1 = %d\n&num2 = %d", &p1, &p2);
+}
+
+int main() {
+	num();
+	int a = *p1 + *p2;
+	printf("\n\n&num1+&num2 = %p = %d \n\n", &a, a);
+	for (int b = 1; b <= a; b++) {
+		for (int c = 1; c < b; c++) {
+			printf("0 ");
+		}
+		printf("%d\n", b);
+	}
+
+	return 0;
+
+}
