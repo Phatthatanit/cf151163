4a2e1290d4f29aa0a2ecdb9fe950c12ccc08094f
PHATTHARANIT TESJAROEN <63010727@KMITL.AC.TH>
2020-11-18 01:03:29 +0700

    a

--- cf151163/cf151163.cpp
+++ cf151163/cf151163.cpp
@@ -1,11 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-int x, y;
-int* p1 = &x;
-int* p2 = &y;
+int x[4];
+int* p1 = &x[1];
+int* p2 = &x[2];
 void num() {
 	printf("input num1 num2 : ");
-	scanf("%d %d", &x, &y);
+	scanf("%d %d", &x[1], &x[2]);
 	printf("&num1 = %d\n&num2 = %d", &p1, &p2);
 }
 
@@ -13,11 +13,11 @@ int main() {
 	num();
 	int a = *p1 + *p2;
 	printf("\n\n&num1+&num2 = %p = %d \n\n", &a, a);
-	for (int b = 1; b <= a; b++) {
-		for (int c = 1; c < b; c++) {
+	for (x[3] = 1; x[3] <= a; x[3]++) {
+		for (x[4] = 1; x[4] < x[3]; x[4]++) {
 			printf("0 ");
 		}
-		printf("%d\n", b);
+		printf("%d\n", x[4]);
 	}
 
 	return 0;
